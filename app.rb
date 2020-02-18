61741e5514869a380ea8e9df136913c102ada837
sroman-537 <sam.roman@kellogg.northwestern.edu>
2020-02-18 16:43:01 +0000

    API added

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
 # enter your Dark Sky API key here
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "0adf9455fa4ccdbcd3cd91b7814d2cbe"
 
 get "/" do
   # show a view that asks for the location
