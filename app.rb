efb5116a629e0754a0693cb9d641eedacbbadc0b
sroman-537 <sam.roman@kellogg.northwestern.edu>
2020-02-19 17:43:57 +0000

    Update for #1

--- app.rb
+++ app.rb
@@ -9,9 +9,22 @@ before { puts "Parameters: #{params}" }
 # enter your Dark Sky API key here
 ForecastIO.api_key = "0adf9455fa4ccdbcd3cd91b7814d2cbe"
 
+forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
+
 get "/" do
-  # show a view that asks for the location
+  view "geocode"
 end
+
+get "/map" do
+    results = Geocoder.search(params["q"])
+    lat_long = results.first.coordinates # => [lat, long]
+    "#{lat_long[0]} #{lat_long[1]}"
+end
+
+for day in forecast["daily"]["data"]
+  puts "A high temperature of #{day["temperatureHigh"]} and #{day["summary"]}."
+end
+
 
 get "/news" do
   # do everything else
